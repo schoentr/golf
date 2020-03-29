32056bcec7cf17d16285bfdc83cede242ec01d49
Tim Schoen <schoentr@gmail.com>
2020-03-28 17:15:01 -0700

    ejs - index working

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ client.on('error', err => console.error(err));
 //Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-
 app.use(
   methodOverride(req => {
     if (req.body && typeof req.body === 'object' && ' _method' in req.body){
@@ -39,19 +38,29 @@ app.use(
   })
 );
 
+// Routes
+app.get('/', homepage);
+
+app.get('*', (req,res) => res.status(404).send('This route does not exist'));
+app.listen(PORT, () => console.log( `Listening on port: ${PORT}`));
+
+function homepage(req,res){
+    res.render('pages/index');
+}
 
 
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res)=> {
-  res.statusCode = 200;
-  res.setHeader('Content-Type','text/plain');
-  res.end('Hello World');    
-});
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = http.createServer((req, res)=> {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type','text/plain');
+//   res.end('Hello World');    
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
